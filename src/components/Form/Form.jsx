32e59c33b1c041f04637cfecbb0f4414523f6502
Leonardo Ferreira Rochedo <learnir@outlook.com.br>
2022-10-16 20:20:29 -0300

    Add form and header, function in form done

---
+++ src/components/Form/Form.jsx
@@ -0,0 +1,43 @@
+import React, { useState } from 'react';
+
+import Zoom from 'react-reveal/Zoom';
+
+import logo from '../../assets/logo.png'
+import { AiOutlineSearch } from 'react-icons/ai'
+
+import './Form.css'
+
+export function Form() {
+
+    const [address, setAddress] = useState('')
+
+    function consultAPI() {
+        console.log("Busca na API!")
+    }
+
+    function keyPress(e) {
+        if (e.key == "Enter") {
+            consultAPI()
+        }
+    }
+
+    return (
+        <div className='container'>
+            <Zoom>
+            <header>
+                <a href="home.persisinternet.com.br" target="_blank"><img src={logo} alt="Logo da Persis" /></a>
+            </header>
+            <div className="form">
+                <p>Busque qual o melhor período seu agendamento!</p>
+                <div className="form-actions">
+                    <input type="text" placeholder='Digite o endereço' onChange={(e) => setAddress(e.target.value)} onKeyUp={(e) => keyPress(e)} />
+                    <button onClick={consultAPI}><AiOutlineSearch size={20}/></button>
+                </div>
+            </div>
+            <div className="result">
+                <h1>ENDEREÇO: {address}</h1>
+            </div>
+            </Zoom>
+        </div>
+    )
+}
