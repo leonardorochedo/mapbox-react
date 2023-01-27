6edc0d0f73caebf92a4118aee27dee3845e9ba0f
Leonardo Ferreira Rochedo <learnir@outlook.com.br>
2023-01-27 14:09:01 +0000

    Try: Create a Layer (Polygon), but not render

--- src/components/Map/Map.jsx
+++ src/components/Map/Map.jsx
@@ -31,7 +31,9 @@ export function Map({ ...props }) {
     container: mapContainer.current,
     style: 'mapbox://styles/mapbox/streets-v12',
     center: [cityPosition.lng, cityPosition.lat],
-    zoom: zoom
+    zoom: zoom,
+    //antialias: true, // create the gl context with MSAA antialiasing, so custom layers are antialiased
+    //projection: 'mercator'
     });
   });
 
@@ -47,7 +49,7 @@ export function Map({ ...props }) {
 
   // Map Marker
   useEffect(() => {
-    const marker = new mapboxgl.Marker()
+    const marker = new mapboxgl.Marker({ color: 'red', rotation: 30 })
       .setLngLat([clientCoord.lng, clientCoord.lat])
       .addTo(map.current);
   }, [address])
@@ -69,6 +71,98 @@ export function Map({ ...props }) {
   }, [address]);
 
   // Polygon
+  // create a custom style layer to implement the WebGL content
+  const highlightLayer = {
+    id: 'highlight',
+    type: 'custom',
+     
+    // method called when the layer is added to the map
+    // https://docs.mapbox.com/mapbox-gl-js/api/#styleimageinterface#onadd
+    onAdd: function (map, gl) {
+    // create GLSL source for vertex shader
+    const vertexSource = `
+    uniform mat4 u_matrix;
+    attribute vec2 a_pos;
+    void main() {
+    gl_Position = u_matrix * vec4(a_pos, 0.0, 1.0);
+    }`;
+     
+    // create GLSL source for fragment shader
+    const fragmentSource = `
+    void main() {
+    gl_FragColor = vec4(1.0, 0.0, 0.0, 0.5);
+    }`;
+     
+    // create a vertex shader
+    const vertexShader = gl.createShader(gl.VERTEX_SHADER);
+    gl.shaderSource(vertexShader, vertexSource);
+    gl.compileShader(vertexShader);
+     
+    // create a fragment shader
+    const fragmentShader = gl.createShader(gl.FRAGMENT_SHADER);
+    gl.shaderSource(fragmentShader, fragmentSource);
+    gl.compileShader(fragmentShader);
+     
+    // link the two shaders into a WebGL program
+    this.program = gl.createProgram();
+    gl.attachShader(this.program, vertexShader);
+    gl.attachShader(this.program, fragmentShader);
+    gl.linkProgram(this.program);
+     
+    this.aPos = gl.getAttribLocation(this.program, 'a_pos');
+     
+    // define vertices of the triangle to be rendered in the custom style layer
+    const helsinki = mapboxgl.MercatorCoordinate.fromLngLat({
+    lng: 25.004,
+    lat: 60.239
+    });
+    const berlin = mapboxgl.MercatorCoordinate.fromLngLat({
+    lng: 13.403,
+    lat: 52.562
+    });
+    const kyiv = mapboxgl.MercatorCoordinate.fromLngLat({
+    lng: 30.498,
+    lat: 50.541
+    });
+     
+    // create and initialize a WebGLBuffer to store vertex and color data
+    this.buffer = gl.createBuffer();
+    gl.bindBuffer(gl.ARRAY_BUFFER, this.buffer);
+    gl.bufferData(
+    gl.ARRAY_BUFFER,
+    new Float32Array([
+    helsinki.x,
+    helsinki.y,
+    berlin.x,
+    berlin.y,
+    kyiv.x,
+    kyiv.y
+    ]),
+    gl.STATIC_DRAW
+    );
+    },
+     
+    // method fired on each animation frame
+    // https://docs.mapbox.com/mapbox-gl-js/api/#map.event:render
+    render: function (gl, matrix) {
+    gl.useProgram(this.program);
+    gl.uniformMatrix4fv(
+    gl.getUniformLocation(this.program, 'u_matrix'),
+    false,
+    matrix
+    );
+    gl.bindBuffer(gl.ARRAY_BUFFER, this.buffer);
+    gl.enableVertexAttribArray(this.aPos);
+    gl.vertexAttribPointer(this.aPos, 2, gl.FLOAT, false, 0, 0);
+    gl.enable(gl.BLEND);
+    gl.blendFunc(gl.SRC_ALPHA, gl.ONE_MINUS_SRC_ALPHA);
+    gl.drawArrays(gl.TRIANGLE_STRIP, 0, 3);
+    }
+  };
+
+  // add the custom style layer to the map
+  // map.addLayer(highlightLayer, 'building');
+
   const zonaSulLeste = [
     { lng: -51.1592102, lat: -23.3116757 },
     { lng: -51.1592102, lat: -23.3810226 },
