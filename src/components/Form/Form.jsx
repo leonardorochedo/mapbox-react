9ef991699160137382840df30af608c61f1dd24b
Leonardo Ferreira Rochedo <learnir@outlook.com.br>
2023-01-28 18:34:20 +0000

    Remove: Persis identify

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -24,12 +24,9 @@ export function Form() {
   return (
     <div className="container">
       <header>
-        <a href="https://home.persisinternet.com.br" target="_blank">
-          <img src={logo} alt="Logo da Persis" />
-        </a>
+        <img src={logo} alt="Logo da Persis" />
       </header>
       <div className="form">
-        <p>Descubra qual o melhor período para seu agendamento!</p>
         <div className="form-actions">
           <input
             type="text"
@@ -46,47 +43,9 @@ export function Form() {
           <br />
           será pego a localização mais próxima
         </p>
-        <h2 className="onUpdate">⚠️👷✋ EM ATUALIZAÇÃO ✋👷⚠️</h2>
       </div>
       <div className="map-table">
         <Map address={addressProp} />
-      </div>
-      <div className="needFixLater">
-        <div className="tecnics">
-          <div className="tec1">
-            <h3>Técnico 1</h3>
-            <p id="soon">Manhã</p>
-            <p id="late">Tarde</p>
-          </div>
-          <div className="tec2">
-            <h3>Técnico 2</h3>
-            <p id="soon">Manhã</p>
-            <p id="late">Tarde</p>
-          </div>
-          <div className="others1">
-            <h3>2 Horários</h3>
-            <ul>
-              <li>Ocupar 2 horários na agenda.</li>
-              <li>Se houver mais de um cliente, 2 horários para apenas 1.</li>
-              <li>
-                Período deve ser o mais próximo do descrito no mapa;
-                <br />
-                Ex: Endereço mais próximo está para tarde, tentar agendar para
-                tarde.
-              </li>
-              <li id="final-li">
-                Preferência para agenda que já tiver agendamento da cidade.
-              </li>
-            </ul>
-            <p id="cambe">Cambé</p>
-            <p id="uniao">União</p>
-            <p id="uniao">Jataizinho</p>
-          </div>
-          <div className="others2">
-            <h3>Fim de Tarde</h3>
-            <p id="ibipora">Ibiporã</p>
-          </div>
-        </div>
       </div>
     </div>
   );
